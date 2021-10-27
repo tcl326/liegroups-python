749af97d9b8984efb97bc1471cec75427f3702d9
tcl326 <lintingche2012@gmail.com>
2021-10-27 16:15:40 +0800

    remove algorithm.py

--- src/liegroups/algorithm.py
+++
@@ -1,9 +0,0 @@
-"""
-Collection of Algorithms performed on Lie group elements
-"""
-
-from liegroups.base import LieGroupBase
-
-
-def interpolate(a: LieGroupBase, b: LieGroupBase, t: float) -> LieGroupBase:
-    pass
